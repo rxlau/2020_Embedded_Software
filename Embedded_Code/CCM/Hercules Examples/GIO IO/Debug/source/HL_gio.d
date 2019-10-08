5a23de75be11be559369da0961037e8c33b78757
Joshua Miller <jemille2@uci.edu>
2019-10-07 17:05:23 -0700

    Added More Hercules Examples
    Added examples for NHET GIO, GIO IO and GIO RTI

---
+++ Embedded_Code/CCM/Hercules Examples/GIO IO/Debug/source/HL_gio.d
@@ -0,0 +1,35 @@
+# FIXED
+
+source/HL_gio.obj: ../source/HL_gio.c
+source/HL_gio.obj: C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_gio.h
+source/HL_gio.obj: C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_reg_gio.h
+source/HL_gio.obj: C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_sys_common.h
+source/HL_gio.obj: C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_hal_stdtypes.h
+source/HL_gio.obj: C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h
+source/HL_gio.obj: C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h
+source/HL_gio.obj: C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h
+source/HL_gio.obj: C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h
+source/HL_gio.obj: C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h
+source/HL_gio.obj: C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h
+source/HL_gio.obj: C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h
+source/HL_gio.obj: C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h
+source/HL_gio.obj: C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdbool.h
+source/HL_gio.obj: C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_sys_vim.h
+source/HL_gio.obj: C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_reg_vim.h
+
+../source/HL_gio.c: 
+C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_gio.h: 
+C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_reg_gio.h: 
+C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_sys_common.h: 
+C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_hal_stdtypes.h: 
+C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h: 
+C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h: 
+C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h: 
+C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h: 
+C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h: 
+C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h: 
+C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h: 
+C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h: 
+C:/Software/TI/CCS/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdbool.h: 
+C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_sys_vim.h: 
+C:/Users/Josh/Documents/Racecar/TI//\ Workspace/GIO//\ IO/include/HL_reg_vim.h: 
