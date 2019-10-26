3bd1910e14b109b6ab9a738edac30e5fb6edfdb4
liuribe <56175414+liuribe@users.noreply.github.com>
2019-10-26 15:45:31 -0700

    TImer
    Initialized the timer stuff and did a rough outline of upcoming code.

--- Embedded_Code/SSA/MSP430 Examples/Wheel Speed.ino
+++ Embedded_Code/SSA/MSP430 Examples/Wheel Speed.ino
@@ -2,7 +2,7 @@
  * Portion: Wheel Speed 
  * This code is used to calculate wheel speed.
  */
-
+#include <msp430.h>
 
 int hall_pin = P2.5;
 int lapse_time = 0; 
@@ -17,36 +17,41 @@ void setup()
   pinMode(hall_pin, INPUT); 
 }
 
+/* function calcs wheel speed */
 int getwheelspeed(rotation_time) 
 {
     wheel_speed_rotation = (rotation_time)/distance_per_rotation; 
     return wheel_speed_rotation;   
-  }
+}
+int Read_Hall() 
+{
+     /* Reads hall sensor*/
+    int read_pin = digitalRead(hall_pin) 
+    return read_pin;
+    
+}
 void loop() 
 {
+    int hall_read =0;
+    int rotation_1_tick = 0;
+    int rotation_2_tick = 0; 
+    TACCR0 = 0;
   /* Start Timer*/
-    hall_read = digitalRead(hall_pin) 
+    WDTCL = WDTPW + WDTHOLD; /* stops the dog watch timer */
+    TACCR0 = 0; /* stopped the timer */
+    TACCTL0 |= CCIE; /* enables the interupt for CCR0 */
+    TACTL = TASSEL_2 + ID_0 + MC_2 /* Picks SMCLK, input divider = 1, Mode: Continuous*/
+    DCOCTL = CALDCO_8MHz;/* Set the DOC clock to 8 MHz*/
+    _enable_interrupt(); 
     
-    /**Magnet is close to sensor  **/
-    if(hall_read == LOW)
-    {
-       sample = sample + 1; 
-      /* End Timer*/ 
-      /* time of half rotation from timer*/
-
-        temp_time =+temp_time; 
-      /*count the sample number*/ 
-      if(sample == 2) 
-      {
-        sample = 0; 
-        /*function to obtain wheel speed*/ 
-        getwheelspeed(temp_time);
-        
-        }
-         
-      
-      } 
-      
+    hall_read = Read_Hall();
+    /* stop timer here  */ 
+    /* Capture the value */ 
+ 
+    roation_1_tick = ; 
+    Read_Hall();
+    
+     
     /*Magnet is away from the sensor */
     else (hall_pin == HIGH) 
     
