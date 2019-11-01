8d092f5af98e62c1e2ff9f65d468185d4c6d5a78
liuribe <56175414+liuribe@users.noreply.github.com>
2019-10-31 18:01:21 -0700

    Created wheel speed function
    Added the wheel speed function that takes in the ticks of the timer and produces speed of the tire. Also added some comments and changed the clock divisions to 8 which in turn causes each tick to equal 1 micro second in time

--- Embedded_Code/SSA/ssa-initial.ino
+++ Embedded_Code/SSA/ssa-initial.ino
@@ -26,21 +26,37 @@ void initiate_Timer()
     WDTCL = WDTPW + WDTHOLD; /* stops the dog watch timer */
     TACCR0 = 0; /* stopped the timer */
     TACCTL0 |= CCIE; /* enables the interupt for CCR0 */
-    TACTL = TASSEL_2 + ID_0 + MC_2 /* Picks SMCLK, input divider = 1, Mode: Continuous*/
+    TACTL = TASSEL_2 + ID_3 + MC_2 /* Picks SMCLK, input divider = 8, Mode: Continuous*/ 
     DCOCTL = CALDCO_8MHz;/* Set the DOC clock to 8 MHz*/
 }
 int end_timer() 
 {
     int temp =0; 
     TACTL = MC_0; /*this will halt the timer */
-    temp = TAR; 
+    temp = TAR; /* this is the number of ticks but I think this is in hex have to convert*/
     return temp; 
      
 }
+int wheel_speed(int half_rot_time)
+{
+  /* the tire radius is 18 inches */
+  int wheel_diameter = 18; 
+  int circumf = 0; 
+  int wheel_speed = 0;
+  int half_rotation = 0; 
+  /* The circumf is the distance for a full rotation */
+  circumf = (3.14159 * wheel_diameter);
+  /* half_rotation is distance for a half rotation*/
+  half_rotation = circumf/2;
+  /* Wheel speed for this half of a rotation*/
+  wheel_speed = half_rotation/ half_rot_time;
+  return wheel_speed; 
+  }
 void loop() 
 {
     int hall_read =0;
     int reading = 0; 
+    int _speed_ = 0;
     int lil_time = 0;
     int ticks = 0;  
   /* initiate Timer*/
@@ -50,13 +66,12 @@ void loop()
         hall_read = Read_Hall();
         if(hall_read == LOW)
         {
-            ticks = end_timer(); //This will end the timer and return the number of ticks 
+            ticks = end_timer(); /*This will end the timer and return the number of ticks, each tick is 1 micro second*/
+            _speed_ = wheel_speed(ticks); /* will give the speed of this rotation*/
             reading = 1; /* will then exit this loop to reset the timer*/
+            
           }
        
 
     }
-    roation_1_tick = ; 
-    Read_Hall();
- 
 }
