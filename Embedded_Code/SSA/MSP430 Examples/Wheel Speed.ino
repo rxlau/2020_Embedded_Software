6ec0f40e80578118fdaae19b29792479b948b90d
liuribe <56175414+liuribe@users.noreply.github.com>
2019-11-02 14:55:03 -0700

    Update Wheel Speed.ino

--- Embedded_Code/SSA/MSP430 Examples/Wheel Speed.ino
+++ Embedded_Code/SSA/MSP430 Examples/Wheel Speed.ino
@@ -5,12 +5,6 @@
 #include <msp430.h>
 
 int hall_pin = P2.5;
-int lapse_time = 0; 
-int wheel_diameter= 0; 
-int sample = 0; 
-int temp_time = 0; 
-int distance_per_rotation = 0; /* Value is set by circumference of the wheel*/
-
 void setup() 
 {
   /* Hall Sensor Setup*/
@@ -18,11 +12,7 @@ void setup()
 }
 
 /* function calcs wheel speed */
-int getwheelspeed(rotation_time) 
-{
-    wheel_speed_rotation = (rotation_time)/distance_per_rotation; 
-    return wheel_speed_rotation;   
-}
+
 int Read_Hall() 
 {
      /* Reads hall sensor*/
@@ -30,29 +20,58 @@ int Read_Hall()
     return read_pin;
     
 }
-void loop() 
+/* Initiates TImer */
+void initiate_Timer() 
 {
-    int hall_read =0;
-    int rotation_1_tick = 0;
-    int rotation_2_tick = 0; 
-    TACCR0 = 0;
-  /* Start Timer*/
     WDTCL = WDTPW + WDTHOLD; /* stops the dog watch timer */
     TACCR0 = 0; /* stopped the timer */
     TACCTL0 |= CCIE; /* enables the interupt for CCR0 */
-    TACTL = TASSEL_2 + ID_0 + MC_2 /* Picks SMCLK, input divider = 1, Mode: Continuous*/
+    TACTL = TASSEL_2 + ID_3 + MC_2 /* Picks SMCLK, input divider = 8, Mode: Continuous*/ 
     DCOCTL = CALDCO_8MHz;/* Set the DOC clock to 8 MHz*/
-    _enable_interrupt(); 
-    
-    hall_read = Read_Hall();
-    /* stop timer here  */ 
-    /* Capture the value */ 
- 
-    roation_1_tick = ; 
-    Read_Hall();
-    
+}
+int end_timer() 
+{
+    int temp =0; 
+    TACTL = MC_0; /*this will halt the timer */
+    temp = TAR; /* this is the number of ticks but I think this is in hex have to convert*/
+    return temp; 
      
-    /*Magnet is away from the sensor */
-    else (hall_pin == HIGH) 
-    
+}
+int wheel_speed(int half_rot_time)
+{
+  /* the tire radius is 18 inches */
+  int wheel_diameter = 18; 
+  int circumf = 0; 
+  int wheel_speed = 0;
+  int half_rotation = 0; 
+  /* The circumf is the distance for a full rotation */
+  circumf = (3.14159 * wheel_diameter);
+  /* half_rotation is distance for a half rotation*/
+  half_rotation = circumf/2;
+  /* Wheel speed for this half of a rotation*/
+  wheel_speed = half_rotation/ half_rot_time;
+  return wheel_speed; 
+  }
+void loop() 
+{
+    int hall_read =0;
+    int reading = 0; 
+    int _speed_ = 0;
+    int lil_time = 0;
+    int ticks = 0;  
+  /* initiate Timer*/
+    initiate_Timer();
+    while(reading != 0) 
+    {
+        hall_read = Read_Hall();
+        if(hall_read == LOW)
+        {
+            ticks = end_timer(); /*This will end the timer and return the number of ticks, each tick is 1 micro second*/
+            _speed_ = wheel_speed(ticks); /* will give the speed of this rotation*/
+            reading = 1; /* will then exit this loop to reset the timer*/
+            
+          }
+       
+
+    }
 }
