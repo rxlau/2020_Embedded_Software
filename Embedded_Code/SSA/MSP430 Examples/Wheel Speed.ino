ed82a6d27d16c50a807f9023b9be92b5dfb2b1d3
liuribe <56175414+liuribe@users.noreply.github.com>
2019-11-15 15:03:15 -0800

    Edits to functions
    Replaced the use of the ACLK timer and used micros() function to get time of sensor going to LOW state. Also implemented an interrupt for the change of state. I was not able to load this to my MSP430

--- Embedded_Code/SSA/MSP430 Examples/Wheel Speed.ino
+++ Embedded_Code/SSA/MSP430 Examples/Wheel Speed.ino
@@ -7,48 +7,29 @@
 
 int Read_Hall();
 int initiateTimer();
-int end_Timer();
+int end_Timer(); 
 int wheel_speed();
 int hall_pin = 13;
+unsigned long initial_time = 0; 
+unsigned long end_time = 0; 
+unsigned long time_difference = 0;
+int w_speed=0;
 
 void setup() 
 {
   /* Hall Sensor Setup*/
   pinMode(hall_pin, INPUT); 
+  /*interrupt setup for the hall sensor to be excecuted in FALLING state*/
+  attachInterrupt(digitalPinToInterrupt(hall_pin),pin_ISR,FALLING);
+  Serial.begin(9600);
 }
 
 /* function calcs wheel speed */
-
-int Read_Hall() 
-{
-     /* Reads hall sensor*/
-    int read_pin = digitalRead(hall_pin);
-    return read_pin;
-    
-}
-/* Initiates TImer */
-void initiate_Timer() 
-{
-    WDTCTL = WDTPW + WDTHOLD; /* stops the dog watch timer */
-    TACCR0 = 0; /* stopped the timer */
-    TACCTL0 |= CCIE; /* enables the interupt for CCR0 */
-    TACTL = TASSEL_2 + ID_3 + MC_2; /* Picks SMCLK, input divider = 8, Mode: Continuous*/ 
-    DCOCTL = CALDCO_8MHZ;/* Set the DOC clock to 8 MHz*/
-}
-int end_timer() 
-{
-    int temp =0; 
-    TACTL = MC_0; /*this will halt the timer */
-    temp = TAR; /* this is the number of ticks but I think this is in hex have to convert*/
-    return temp; 
-     
-}
 int wheel_speed(int half_rot_time)
 {
   /* the tire radius is 18 inches */
   int wheel_diameter = 18; 
-  int circumf = 0; 
-  int wheel_speed = 0;
+  int circumf = 0;
   int half_rotation = 0; 
   /* The circumf is the distance for a full rotation */
   circumf = (3.14159 * wheel_diameter);
@@ -57,28 +38,27 @@ int wheel_speed(int half_rot_time)
   /* Wheel speed for this half of a rotation*/
   wheel_speed = half_rotation/ half_rot_time;
   return wheel_speed; 
-  }
-  
+}
+
+/* Interrupt function to calculate the wheel speed */
+void pin_ISR()
+{
+  end_time = micros(); 
+ Serial.print("End Time"); /* Testing purpose for engineer to see initial time taken*/
+ Serial.println(end_time); /* Testing purpose for engineer to see initial time taken*/
+ delay(10); /* Testing purpose for engineer to see initial time taken*/
+  time_difference = end_time - initial_time;
+  Serial.print("Time Difference"); /* Testing purpose for engineer to see initial time taken*/
+  Serial.println(time_difference);/* Testing purpose for engineer to see initial time taken*/ 
+  delay(10);/* Testing purpose for engineer to see initial time taken*/
+  w_speed = wheel_speed(time_difference);
+}
+
+/* Start the timer in the loop */
 void loop() 
 {
-    int hall_read =0;
-    int reading = 0; 
-    int _speed_ = 0;
-    int lil_time = 0;
-    int ticks = 0;  
-  /* initiate Timer*/
-    initiate_Timer();
-    while(reading = 0) 
-    {
-        hall_read = Read_Hall();
-        if(hall_read == LOW)
-        {
-            ticks = end_timer(); /*This will end the timer and return the number of ticks, each tick is 1 micro second*/
-            _speed_ = wheel_speed(ticks); /* will give the speed of this rotation*/
-            reading = 1; /* will then exit this loop to reset the timer*/
-            Serial.print(_speed_,DEC);
-          }
-       
-
-    }
+  initial_time = micros();
+  Serial.print("Initial Time:");/* Testing purpose for engineer to see initial time taken*/
+  Serial.println(initial_time);/* Testing purpose for engineer to see initial time taken*/
+  delay(10);/* Testing purpose for engineer to see initial time taken*/
 }
