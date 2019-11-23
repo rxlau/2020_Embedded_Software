f045497aee2b3fa37798869cb93c88e19bb5b725
Jada Berenguer <51301363+berenguj@users.noreply.github.com>
2019-11-23 12:49:58 -0800

    Add files via upload

--- Embedded_Code/SSA/MSP430 Examples/Wheel Speed/wheelspeed.ino
+++ Embedded_Code/SSA/MSP430 Examples/Wheel Speed/wheelspeed.ino
@@ -29,7 +29,7 @@ void setup()
 }
 
 /* function calcs wheel speed */
-float Wheel_speed(float time_difference)
+float Wheel_speed(float time_difference_micro)
 {
   /* the tire radius is 18 inches */
   float wheel_diameter = 18;
@@ -37,22 +37,37 @@ float Wheel_speed(float time_difference)
   float half_rotation = 0;
   float time_in_millisecs = 0;
   float wheel_speed_f = 0;
+  float hours = 0;
+  float miles = 0;
   /* The circumf is the distance for a full rotation */
   circumf = (3.14159 * wheel_diameter);
   /* half_rotation is distance for a half rotation*/
   half_rotation = circumf/2;
-  /*microseconds to seconds*/
-  time_in_millisecs = time_difference / 1000;
-  //Serial.print("half rot time");
-  //Serial.println(half_rot_time);
-  Serial.print("timeinmillisecs: ");
-  Serial.println(time_in_millisecs, 4);
-  /* Wheel speed for this half of a rotation*/
-  /*in half rotations / millisecond*/
+
+  /*time diff in microseconds*/
+  Serial.print("time diff micro: ");
+  Serial.println(time_difference_micro);
+
+  /*microseconds to hours conversion*/
+  float power = pow(10,9);
+  power = power * 3.6;
+  hours = time_difference_micro / power;
+  Serial.print("hours conversion: ");
+  Serial.println(hours, 5);
+
+  /*half rotation*/
   Serial.print("half rotation: ");
   Serial.println(half_rotation); 
-  wheel_speed_f = half_rotation / time_in_millisecs;
-  Serial.print("wheel speed f: ");
+  
+  /*half rotations to miles*/
+  miles = circumf / 63360;
+  Serial.print("miles: ");
+  Serial.println(miles, 5);
+  
+  /*Wheel speed for this half of a rotation*/
+  /*convert from rotations per microsecond to miles per hour*/
+  wheel_speed_f = miles / hours;
+  Serial.print("wheel speed in miles per hours: ");
   Serial.println(wheel_speed_f, 4);
   
   return wheel_speed_f;
@@ -85,6 +100,8 @@ void loop()
       wheel_speed_final= Wheel_speed(time_difference);
       Serial.print("wheel speed in main: ");
       Serial.println(wheel_speed_final, 4);
+      Serial.println(" ");
+      Serial.println(" "); 
       KV = 0;// resetting variable to 0 to take the initial time 
     }
    
